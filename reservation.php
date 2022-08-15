676a8fac1911e586007f0abfb18452de61024c0a
nowratun oyshe <nowratunoyshe199807@gmail.com>
2022-08-15 20:08:54 +0600

    added some page

---
+++ reservation.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Reservation</title>
+    <!-- Required meta tags -->
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- swiper css link  -->
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet">
+
+
+    <!--Custom CSS-->
+    <link rel="stylesheet" href="css/style.css">
+
+    <!--font family-->
+    <link rel="preconnect" href="https://fonts.googleapis.com/%22%3E">
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300&display=swap" rel="stylesheet">
+
+    <!---->
+</head>
+
+<body>
+
+<!-- header section starts  -->
+
+<section class="header">
+
+    <a href="home.php" class="logo">MyEventPlanner</a>
+ 
+    <nav class="navbar">
+       <a href="home.php">Home</a>
+       <a href="about.php">About Us</a>
+       <a href="services.php">Services</a>
+       <a href="gallery.php">Gallery</a>
+       <a href="combo.php">Combo</a>
+       <a href="reservation.php">Reservation</a>
+    </nav>
+
+ </section>
+ 
+
+ <div class="heading" style="background:url(images/reservation.jpg) no-repeat">
+   <h1>Create Your Event</h1>
+</div>
+
+
+ <!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+ 
+       <div class="box">
+          <h3>quick links</h3>
+          <a href="home.php" style="color:#ffb9ff"><i class="fas fa-angle-right"></i>Home</a>
+          <a href="about.php" style="color:#ffb9ff"><i class="fas fa-angle-right"></i>About Us</a>
+          <a href="services.php" style="color:#ffb9ff"><i class="fas fa-angle-right"></i>Services</a>
+          <a href="gallery.php" style="color:#ffb9ff"><i class="fas fa-angle-right"></i>Gallery</a>
+          <a href="combo.php" style="color:#ffb9ff"><i class="fas fa-angle-right"></i>Combo</a>
+          <a href="reservation.php" style="color:#ffb9ff"><i class="fas fa-angle-right"></i>Reservation</a>
+       </div>
+ 
+       <div class="box">
+          <h3>extra links</h3>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-angle-right"></i> ask questions</a>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-angle-right"></i> about us</a>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-angle-right"></i> privacy policy</a>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-angle-right"></i> terms of use</a>
+       </div>
+ 
+       <div class="box">
+          <h3>contact info</h3>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-phone"></i> +123-456-7890 </a>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-phone"></i> +111-222-3333 </a>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-envelope"></i> shaikhanas@gmail.com </a>
+          <a href="#" style="color:#ffb9ff"> <i class="fas fa-map"></i> mumbai, india - 400104 </a>
+       </div>
+ 
+       <div class="box">
+          <h3>follow us</h3>
+          <a href="#" style="color:#ffb9ff"> <i class="fab fa-facebook-f"></i> facebook </a>
+          <a href="#" style="color:#ffb9ff"> <i class="fab fa-twitter"></i> twitter </a>
+          <a href="#" style="color:#ffb9ff"> <i class="fab fa-instagram"></i> instagram </a>
+          <a href="#" style="color:#ffb9ff"> <i class="fab fa-linkedin"></i> linkedin </a>
+       </div>
+ 
+    </div>
+ 
+    <div class="credit"> created by <span>Jaasia & Oyshe</span> | all rights reserved! </div>
+ 
+ </section>
+ 
+ <!-- footer section ends -->
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+</body>
+</html>
