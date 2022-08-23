c65e256279f81668bc46e4f55cbdcfe0e3cfaae8
Jaasia <jaasiaa2019@gmail.com>
2022-08-23 17:14:36 +0600

    Form done

--- gallery.php
+++ gallery.php
@@ -45,7 +45,7 @@
  </section>
  
 
- <div class="heading">
+ <div class="heading" style="background:url(images/optionsBG.jpg) no-repeat">
    <h1>Gallery</h1>
 </div>
 
--- reservation.php
+++ reservation.php
@@ -46,8 +46,79 @@
  
 
  <div class="heading" style="background:url(images/optionsBG.jpg) no-repeat">
-   <h1>Create Your Event</h1>
+   <h1>Book Your Event</h1>
 </div>
+
+
+
+
+<!-- booking section starts  -->
+
+<section class="booking">
+
+   <h1 class="heading-title">Book your Event</h1>
+
+   <form action="book_form.php" method="post" class="book-form">
+
+      <div class="flex">
+         <div class="inputBox">
+            <span>name :</span>
+            <input type="text" placeholder="enter your name" name="name">
+         </div>
+         <div class="inputBox">
+            <span>email :</span>
+            <input type="email" placeholder="enter your email" name="email">
+         </div>
+         <div class="inputBox">
+            <span>phone :</span>
+            <input type="number" placeholder="enter your number" name="phone">
+         </div>
+         <div class="inputBox">
+            <span>address :</span>
+            <input type="text" placeholder="enter your address" name="address">
+         </div>
+         <div class="inputBox">
+            <span>Event Type :</span>
+            <select placeholder="event type" name="type">
+               <option value="Wedding">Wedding</option>
+               <option value="Birthday">Birthday</option>
+               <option value="Mehfil">Mehfil</option>
+               <option value="Party">Party</option>
+               <option value="Others">Others</option>
+            </select>
+         </div>
+         <div class="inputBox">
+            <span>how many :</span>
+            <input type="number" placeholder="number of guests" name="guests">
+         </div>
+         <div class="inputBox">
+            <span>Event Date :</span>
+            <input type="date" name="date of event">
+         </div>
+
+         <div class="inputBox">
+            <span>Event Time :</span>
+            <select>
+               <option value="Lunch">Lunch</option>
+               <option value="Dinner">Dinner</option>
+               <option value="Afternoon">Afternoon</option>
+            </select>
+         </div>
+         
+      </div>
+
+      <input type="submit" value="submit" class="btn" name="send">
+
+   </form>
+
+</section>
+
+<!-- booking section ends -->
+
+
+
+
+
 
 
  <!-- footer section starts  -->
